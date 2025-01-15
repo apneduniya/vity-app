0137604eb343b80187b937d0b2d3bcde3b9d9a71
thatsmeadarsh <thatsmeadarshgupta@gmail.com>
2025-01-15 16:52:30 +0530

    feat: Changed the button style of request new tools and made toolkit page responsive

--- src/app/toolkit/page.tsx
+++ src/app/toolkit/page.tsx
@@ -1,8 +1,8 @@
-import OutlineButton from "@/components/button/Outline";
 import { constructMetaData } from "@/lib/metadata";
 import { HammerIcon } from "lucide-react";
 import Link from "next/link";
 import { ToolkitComponent } from "@/components/page/toolkit";
+import { Button } from "@/components/ui/button";
 
 
 export const metadata = constructMetaData({
@@ -12,21 +12,23 @@ export const metadata = constructMetaData({
 
 
 export default function Toolkit() {
-
     return (
         <>
-            <div className="pt-10 pb-8 px-8 flex w-full justify-between items-end border-b-[.5px] border-gray-600">
+            <div className="w-full pt-10 pb-8 px-8 gap-5 flex flex-col md:flex-row items-center md:justify-between md:items-end border-b-[.5px] border-gray-600">
                 <div>
-                    <h1 className="font-extrabold text-3xl flex items-center">
+                    <h1 className="font-extrabold flex items-center text-2xl md:text-3xl">
                         <HammerIcon className="w-8 h-8 inline-block mr-2" />
                         Toolkit
                     </h1>
-                    <p className="text-gray-400 mt-2 text-xl">
+                    <p className="text-gray-400 mt-2 text-base md:text-lg lg:text-xl">
                         Vity Toolkit is a powerful tool library of web2 and web3 apps for LLMs & AI agents (also known as function calling).
                     </p>
                 </div>
                 <Link href="https://t.me/vitytoolkit/2">
-                    <OutlineButton text="Request new tool" />
+                    {/* <OutlineButton text="Request new tool" /> */}
+                    <Button variant="outline" className="bg-transparent">
+                        Request new tool
+                    </Button>
                 </Link>
             </div>
             <ToolkitComponent />
